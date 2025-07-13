7710c8812c8ccff8ea2a0664cc593b2145c542f4
tifufu <dematasjoshua@hotmail.com>
2025-07-13 18:07:00 +0200

    chore(lexer): added test case

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,32 +8,65 @@ import (
 )
 
 func TestNextToken(t *testing.T) {
-	input := `<div class="p-4">mino</div>`
+	t.Run(`<div class="p-4">mino</div>`, func(t *testing.T) {
+		input := `<div class="p-4">mino</div>`
 
-	tests := []struct {
-		expectedType token.TokenType
-	}{
-		{token.LEFT_CHEV},
-		{token.IDENT},
-		{token.IDENT},
-		{token.ASSIGN},
-		{token.QUOTE},
-		{token.TEXT},
-		{token.QUOTE},
-		{token.RIGHT_CHEV},
-		{token.TEXT},
-		{token.LEFT_CHEV},
-		{token.SLASH},
-		{token.IDENT},
-		{token.RIGHT_CHEV},
-		{token.EOF},
-	}
+		tests := []struct {
+			expectedType token.TokenType
+		}{
+			{token.LEFT_CHEV},
+			{token.IDENT},
+			{token.IDENT},
+			{token.ASSIGN},
+			{token.QUOTE},
+			{token.TEXT},
+			{token.QUOTE},
+			{token.RIGHT_CHEV},
+			{token.TEXT},
+			{token.LEFT_CHEV},
+			{token.SLASH},
+			{token.IDENT},
+			{token.RIGHT_CHEV},
+			{token.EOF},
+		}
 
-	l := NewLexer(input)
-	for i, tt := range tests {
-		tok := l.NextToken()
-		assert.Equal(t, tt.expectedType, tok.Type, "Token idx %d", i)
-	}
+		l := NewLexer(input)
+		for i, tt := range tests {
+			tok := l.NextToken()
+			assert.Equal(t, tt.expectedType, tok.Type, "Token idx %d", i)
+		}
+	})
+
+	t.Run(`<div><span>Meep</span></div>`, func(t *testing.T) {
+		input := `<div><span>Meep</span></div>`
+
+		tests := []struct {
+			expectedType token.TokenType
+		}{
+			{token.LEFT_CHEV},
+			{token.IDENT},
+			{token.RIGHT_CHEV},
+			{token.LEFT_CHEV},
+			{token.IDENT},
+			{token.RIGHT_CHEV},
+			{token.TEXT},
+			{token.LEFT_CHEV},
+			{token.SLASH},
+			{token.IDENT},
+			{token.RIGHT_CHEV},
+			{token.LEFT_CHEV},
+			{token.SLASH},
+			{token.IDENT},
+			{token.RIGHT_CHEV},
+			{token.EOF},
+		}
+
+		l := NewLexer(input)
+		for i, tt := range tests {
+			tok := l.NextToken()
+			assert.Equal(t, tt.expectedType, tok.Type, "Token idx %d", i)
+		}
+	})
 }
 
 func TestTxtToken(t *testing.T) {
