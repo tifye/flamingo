bcb5fa78a79050d777d88654d414d4b49ff9452b
tifufu <dematasjoshua@hotmail.com>
2025-07-13 23:00:31 +0200

    chore(ast): initial ast walk impl

---
+++ ast/walk.go
@@ -0,0 +1,34 @@
+package ast
+
+type Visitor interface {
+	Visit(node Node) (w Visitor)
+}
+
+func walkList[N Node](v Visitor, list []N) {
+	for _, node := range list {
+		Walk(v, node)
+	}
+}
+
+func Walk(v Visitor, node Node) {
+	if v = v.Visit(node); v == nil {
+		return
+	}
+
+	switch n := node.(type) {
+	case *Root:
+		Walk(v, n.Fragment)
+	case *Fragment:
+		walkList(v, n.Nodes)
+	case *Component:
+		Walk(v, n.Name)
+		walkList(v, n.Attrs)
+		walkList(v, n.Nodes)
+	case *Attr:
+		Walk(v, n.Name)
+	case *Text:
+	case *Ident:
+	default:
+		return
+	}
+}
