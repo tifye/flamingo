cb2307ce2f8fbe3aa1059cf5b2ad1f6d869bd4aa
tifufu <dematasjoshua@hotmail.com>
2025-07-31 23:04:54 +0200

    refactor: removed TokenLit from Node interface Nodes are not just comprised of a single token

--- ast/ast.go
+++ ast/ast.go
@@ -7,7 +7,6 @@ const (
 )
 
 type Node interface {
-	TokenLit() string
 	// Pos() token.Pos
 }
 
@@ -45,23 +44,6 @@ type (
 		Lit string
 	}
 )
-
-func (c *Component) TokenLit() string { return c.Name.Name }
-func (i *Ident) TokenLit() string     { return i.Name }
-func (t *Text) TokenLit() string      { return t.Lit }
-func (a *Attr) TokenLit() string      { return a.Name.Name + " " + a.ValueLit }
-func (r *Fragment) TokenLit() string {
-	if len(r.Nodes) > 0 {
-		return r.Nodes[0].TokenLit()
-	}
-	return ""
-}
-func (r *File) TokenLit() string {
-	if r.Fragment != nil {
-		return r.Fragment.TokenLit()
-	}
-	return ""
-}
 
 // elementNode() makes sure that only element nodes can be assigned to an Element
 func (*Component) elementNode() {}
