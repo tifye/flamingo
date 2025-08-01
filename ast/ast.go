0db1fa2f4eb1a27b866bf0e87497949cf42f15cc
tifufu <dematasjoshua@hotmail.com>
2025-08-01 04:49:41 +0200

    feat: added end source code locations for all AST nodes

--- ast/ast.go
+++ ast/ast.go
@@ -14,6 +14,7 @@ const (
 
 type Node interface {
 	Pos() source.Pos
+	End() source.Pos
 }
 
 type RenderNode interface {
@@ -80,6 +81,31 @@ func (n *Element) Pos() source.Pos   { return n.LeftChevron }
 func (n *Ident) Pos() source.Pos     { return n.Position }
 func (n *Attribute) Pos() source.Pos { return n.Name.Pos() }
 func (n *Text) Pos() source.Pos      { return n.Position }
+
+func (n *File) End() source.Pos {
+	if n.Fragment != nil {
+		return n.Fragment.End()
+	}
+	if n.CodeBlock != nil {
+		return n.CodeBlock.TopFence
+	}
+	panic("node has no source location")
+}
+func (n *CodeBlock) End() source.Pos { return n.BottomFence }
+func (n *Fragment) End() source.Pos {
+	l := len(n.Nodes)
+	assert.Assert(l > 0, "fragment has no children")
+	return n.Nodes[l-1].End()
+}
+func (n *Element) End() source.Pos { return n.RightChevron }
+func (n *Ident) End() source.Pos   { return source.Pos(int(n.Position) + len(n.Name)) }
+func (n *Attribute) End() source.Pos {
+	if l := len(n.ValueLiteral); l > 0 {
+		return source.Pos(int(n.Name.Pos()) + l + 1)
+	}
+	return n.Name.End()
+}
+func (n *Text) End() source.Pos { return source.Pos(int(n.Position) + len(n.Literal)) }
 
 // elementNode() makes sure that only element nodes can be assigned to an Element
 func (*Element) elementNode()  {}
